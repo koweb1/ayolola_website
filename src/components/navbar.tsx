b18257d4933fe9546c17d0cd6585a88ea4fddb67
akto <oguntoyinbokorede@gmail.com>
2026-06-16 16:17:27 +0100

    third change

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,32 +1,38 @@
-import "./navbar.css"
+import "./navbar.css";
 import { LuCalendarDays } from "react-icons/lu";
-import logo from "../assets/logo.png"
-export default function NavBar(){
-    return (
-      <div className="navbar-container">
-        <nav>
-          <div className="navbar-logo">
-            <img src={logo} alt="" />
-            <div className="navbar-logo-text">
-              <h4>AYOLOLA MOTOR'S LIMITED </h4>
-              <span>TOYOTA & VOLKSWAGEN</span>
-            </div>
-          </div>
+import { RiMenu3Line } from "react-icons/ri";
+import logo from "../assets/logo.png";
 
-          <ul>
-            <li>Home</li>
-            <li>Services</li>
-            <li>How It Work</li>
-            <li>About Us</li>
-            <li>Contact</li>
-          </ul>
-
-          <div className="nav-book-appointment">
-            <button>
-              <LuCalendarDays size={22} /> Book an Appointment
-            </button>
+export default function NavBar() {
+  return (
+    <div className="navbar-container">
+      <nav>
+        <div className="navbar-logo">
+          <img src={logo} alt="" />
+          <div className="navbar-logo-text">
+            <h4>AYOLOLA MOTOR'S LIMITED</h4>
+            <span>TOYOTA & VOLKSWAGEN</span>
           </div>
-        </nav>
-      </div>
-    );
+        </div>
+
+        <ul>
+          <li>Home</li>
+          <li>Services</li>
+          <li>How It Work</li>
+          <li>About Us</li>
+          <li>Contact</li>
+        </ul>
+
+        <div className="nav-book-appointment">
+          <button>
+            <LuCalendarDays size={22} /> Book an Appointment
+          </button>
+        </div>
+
+        <div className="navbar-hamburger">
+          <RiMenu3Line size={28} />
+        </div>
+      </nav>
+    </div>
+  );
 }
