dd509eb7f039fd40c43a3664de773d114cc3f09f
akto <oguntoyinbokorede@gmail.com>
2026-06-20 13:41:11 +0100

    improved version

--- src/components/service/HeroSection.tsx
+++ src/components/service/HeroSection.tsx
@@ -13,36 +13,33 @@ export default function HeroSection() {
   return (
     <section className="services-hero">
       <div className="services-hero-content">
-        <motion.div
-          className="services-hero-left"
-          variants={fadeUp}
-          initial="hidden"
-          animate="visible"
-        >
-          <span className="services-hero-small-title">OUR SERVICES</span>
+        <div className="services-hero-left">
+          <motion.div variants={fadeUp} initial="hidden" animate="visible">
+            <span className="services-hero-small-title">OUR SERVICES</span>
 
-          <h1>
-            COMPLETE AUTO CARE
-            <span> SERVICES</span>
-          </h1>
+            <h1>
+              COMPLETE AUTO CARE
+              <span> SERVICES</span>
+            </h1>
 
-          <p>
-            Toyota & Volkswagen specialists providing diagnostics, repairs,
-            maintenance and expert vehicle care.
-          </p>
+            <p>
+              Toyota & Volkswagen specialists providing diagnostics, repairs,
+              maintenance and expert vehicle care.
+            </p>
 
-          <div className="services-hero-buttons">
-            <button className="hero-book-btn">
-              <LuCalendarDays size={18} />
-              Book an Appointment
-            </button>
+            <div className="services-hero-buttons">
+              <button className="hero-book-btn">
+                <LuCalendarDays size={18} />
+                Book an Appointment
+              </button>
 
-            <button className="hero-call-btn">
-              <IoCallOutline size={18} />
-              Call Us Now
-            </button>
-          </div>
-        </motion.div>
+              <button className="hero-call-btn">
+                <IoCallOutline size={18} />
+                Call Us Now
+              </button>
+            </div>
+          </motion.div>
+        </div>
 
         <div className="services-hero-right">
           <motion.div
